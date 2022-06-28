3ddee6ae786c29f86ea975e65eb0ad63538a351b
Aline do Amaral <alinedoamaral@live.com>
2022-06-28 11:48:14 -0300

    inputs da página de contratação adicionados

--- src/pages/PaginaContratar.js
+++ src/pages/PaginaContratar.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
+import Cards from './Cards'
 
 export default class PaginaContratar extends Component {
   render() {
     return (
       <div>
-        PaginaContratar
-        <button onClick={this.props.irPaginaHome}>Voltar</button>
+        <div>
+          PaginaContratar
+          <button onClick={this.props.irPaginaHome}>Voltar</button>
+        </div>
+        <div>
+          <input placeholder="Valor Mínimo" type='number' />
+          <input placeholder="Valor Máximo" type='number' />
+          <input placeholder="Busca por título ou descrição" type='text' />
+          <select>
+            <option>Sem Ordenação</option>
+            <option>Menor Valor</option>
+            <option>Maior Valor</option>
+            <option>Título</option>
+            <option>Prazo</option>
+            </select>
+        </div>
+        <Cards/>
+           
       </div>
     );
   }
