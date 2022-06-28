263c816715e69a8ac1c5a4d83e71546a5ef21088
Joclelson <jocleelson.159@hotmail.com>
2022-06-28 16:03:31 -0300

    Implementação do header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { HeaderContainer } from "./styled"
+
+const Header = () => {
+    return (
+        <HeaderContainer>
+            <h2>LabeNinjas</h2>
+            <div>
+                <button>Home</button>
+                <button>Carrinho</button>
+            </div>
+        </HeaderContainer>
+    )
+}
+
+export default Header
---
+++ src/components/Header/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+
+export const HeaderContainer = styled.div `
+    height: 10vh;
+    background-color: gray;
+    padding: 0 12px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+`;
+    
