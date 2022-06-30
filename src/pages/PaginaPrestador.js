a7ca2b58c6995595984cd0361bc2433e348eeae8
Joclelson <jocleelson.159@hotmail.com>
2022-06-30 16:46:46 -0300

    pagina criar serviço funcionando

--- src/pages/PaginaPrestador.js
+++ src/pages/PaginaPrestador.js
@@ -2,113 +2,111 @@ import React, { Component } from "react";
 import Header from "../components/Header/Header";
 import axios from "axios";
 
+
 export default class PaginaPrestador extends Component {
   state = {
     cargo: "",
     descricao: "",
-    preco: "",
-    pagamento: "",
-    data: ""
-  }
+    preco: 0,
+    pagamento: [],
+    data: "",
+  };
 
   postJobs = () => {
     const body = {
       title: this.state.cargo,
       description: this.state.descricao,
-      price: this.state.preco,
+      price: Number(this.state.preco),
       paymentMethods: this.state.pagamento,
       dueDate: this.state.data
-    }
+    };
 
-    axios.post(`https://labeninjas.herokuapp.com/jobs`, body, {
-      headers: {
-        Authorization: "e537021a-ccc6-4427-a00f-e0a8e2d88c59"
-      }
-    }
-    )
+    axios
+      .post(`https://labeninjas.herokuapp.com/jobs`, body, {
+        headers: {
+          Authorization: "e537021a-ccc6-4427-a00f-e0a8e2d88c59",
+        },
+      })
       .then((response) => {
-        console.log(response)
-          ;
+        alert("Cadastrado com Sucesso")
       })
       .catch((error) => {
-        console.log(error);
+        alert(error.message);
       });
   };
 
-
   pegaValorCargo = (event) => {
-    this.setState({
-      cargo: event.target.value
-    })
-  }
+    this.setState({cargo: event.target.value});
+  };
 
   pegaValorDescricao = (event) => {
-    this.setState({
-      descricao: event.target.value
-    })
-  }
+    this.setState({descricao: event.target.value});
+  };
 
   pegaValorPreco = (event) => {
-    this.setState({
-      preco: event.target.value
-    })
-  }
+    this.setState({preco: event.target.value});
+  };
 
   pegaValorPagamento = (event) => {
-    this.setState({
-      pagamento: event.target.value
-    })
-  }
+    let value = Array.from(event.target.selectedOptions, option => option.value)
+    this.setState({pagamento: value});
+  };
 
   pegaValorData = (event) => {
-    this.setState({
-      data: event.target.value
-    })
+    this.setState({data: event.target.value});
+  };
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <main>
+          <h1>Cadastre o seu serviço</h1>
+          
+            <input
+              value={this.state.cargo}
+              onChange={this.pegaValorCargo}
+              placeholder="Cargo"
+            />
+
+            <input
+              value={this.state.descricao}
+              onChange={this.pegaValorDescricao}
+              placeholder="Descrição"
+            />
+
+            <input
+              value={this.state.preco}
+              onChange={this.pegaValorPreco}
+              placeholder="Valor"
+              type='number'
+            />
+
+            <select
+              multiple
+              value={this.state.pagamento}
+              onChange={this.pegaValorPagamento}
+            >
+              <option value="Cartão de Crédito">Cartão de Crédito</option>
+              <option value="Cartão de Débito">Cartão de Débito</option>
+              <option value="Pix">Pix</option>
+              <option value="PayPal">PayPal</option>
+              <option value="Boleto">Boleto</option>
+            </select>
+
+            <input
+              value={this.state.data}
+              onChange={this.pegaValorData}
+              type="date"
+            />
+
+            <button onClick={this.postJobs}>
+              Enviar
+            </button>
+
+          <button onClick={this.props.irPaginaHome}>Voltar</button>
+        </main>
+      </div>
+    );
   }
-
-render() {
-
-  return (
-    <div>
-      <Header />
-      <main>
-
-        <h1>Cadastre o seu serviço</h1>
-        <div>
-
-          <input value={this.state.cargo}
-            onChange={this.pegaValorCargo}
-            placeholder="Cargo" />
-
-          <input value={this.state.descricao}
-            onChange={this.pegaValorDescricao}
-            placeholder="Descrição" />
-
-          <input value={this.state.preco}
-            onChange={this.pegaValorPreco}
-            placeholder="Valor" />
-
-          <select value={this.state.pagamento}
-            onChange={this.pegaValorPagamento}>
-            <option value="Cartão de Crédito">Cartão de Crédito</option>
-            <option value="Cartão de Débito">Cartão de Débito</option>
-            <option value="Pix">Pix</option>
-            <option value="PayPal">PayPal</option>
-            <option value="Boleto">Boleto</option>
-          </select>
-
-          <input
-            value={this.state.data}
-            onChange={this.pegaValorData}
-            type="date" />
-
-          <button onClick={this.postJobs} type="submit">Enviar</button>
-
-
-        </div>
-        <button onClick={this.props.irPaginaHome}>Voltar</button>
-      </main>
-    </div>
-  );
-}
 }
