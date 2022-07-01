36320387e053ffc6332f99d13f608aa8de9b8f39
Joclelson <jocleelson.159@hotmail.com>
2022-07-01 14:01:50 -0300

    Implementação do carrinho

--- src/pages/PaginaCarrinho.js
+++ src/pages/PaginaCarrinho.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react'
 
 export default class PaginaCarrinho extends Component {
   render() {
+    const carrinho = this.props.carrinho.map((job) => {
+      return <div key={job.id}>
+        <p>{job.title}</p>
+        <p>{job.price}</p>
+      </div>
+    })
     return (
       <div>
         <button>
             Finalizar Compra
         </button>
+        {carrinho}
         <button onClick={this.props.irPaginaContratar}>
             Voltar para a Lista
         </button>
